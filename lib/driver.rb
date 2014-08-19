9fec9903105e65460c8f9b4dacd8edc7190c22c7
Mary Gomez <Mgomez@Marys-MacBook-Pro.local>
2014-08-19 19:46:36 +1000

    Removed the old driver files :pear: Mary, Charles, Aaron

--- lib/driver.rb
+++
@@ -1,5 +0,0 @@
-class Driver
-    def honk
-        'honk honk toot toot beep beep'
-    end
-end
--- spec/driver_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'driver'
-
-RSpec.describe Driver do
-  it "should honk" do
-    d = Driver.new
-    expect(d.honk).to eq('honk honk toot toot beep beep')
-  end
-end
