6491f7a802da21a59f740666e96fb9942bbf0f4c
Charles <charles.he@triptra.com>
2014-08-26 19:03:46 +1000

    Refactoring for the move_forward and taxi object

--- spec/taxi_spec.rb
+++ spec/taxi_spec.rb
@@ -9,73 +9,74 @@ describe Taxi do
     		expect(position).to eql([1,1,:W])
     	end
     end
+    
+    describe 'move forward' do
 
-    context "at position 1,1,N" do
-        before :each do
-            @taxi = Taxi.new(1,1,:N)
-        end
+    	context "at position 1,1,N" do
+    	
+    		let(:taxi){ Taxi.new(1,1,:N) }
 
-        it 'should be at the start position we create it at' do
+    	    it 'should be at the start position we create it at' do
 
-            # act
-            location = @taxi.position
+    	        # act
+    	        location = taxi.position
 
-            # assert
-            expect(location).to eql([1,1,:N])
-        end
+    	        # assert
+    	        expect(location).to eql([1,1,:N])
+    	    end
 
-        it 'should be at the end position after it moves' do
+    	    it 'should be at the end position after it moves' do
 
-            # act
-            @taxi.move_forward
+    	        # act
+    	        taxi.move_forward
 
-            # assert
-            expect(@taxi.position).to eql([1,2,:N])
-        end
+    	        # assert
+    	        expect(taxi.position).to eql([1,2,:N])
+    	    end
 
-        it 'should be at (1,3,N) after 2 moves' do
-            # act
-            @taxi.move_forward
-            @taxi.move_forward
+    	    it 'should be at (1,3,N) after 2 moves' do
+    	        # act
+    	        taxi.move_forward
+    	        taxi.move_forward
 
-            # assert
-            expect(@taxi.position).to eql([1,3,:N])
-        end
+    	        # assert
+    	        expect(taxi.position).to eql([1,3,:N])
+    	    end
 
-        it 'should return (1,3,N) after 2 moves' do
-            #act
-            @taxi.move_forward
-            returned_position = @taxi.move_forward
+    	    it 'should return (1,3,N) after 2 moves' do
+    	        #act
+    	        taxi.move_forward
+    	        returned_position = taxi.move_forward
 
-            #assert
-            expect(returned_position).to eql([1,3,:N])
-        end
-    end
+    	        #assert
+    	        expect(returned_position).to eql([1,3,:N])
+    	    end
+    	end
 
-    context "at position 1,1,E" do
+    	context "at position 1,1,E" do
 
-        before :each do
-            @taxi = Taxi.new(1,1,:E)
-        end
+			let (:taxi) {Taxi.new(1,1,:E)}
+    	    
 
-        it 'should be at (2,1,E) after one move' do
+    	    it 'should be at (2,1,E) after one move' do
 
-            # act
-            @taxi.move_forward
+    	        # act
+    	        taxi.move_forward
 
-            # assert
-            expect(@taxi.position).to eql([2,1,:E])
-        end
+    	        # assert
+    	        expect(taxi.position).to eql([2,1,:E])
+    	    end
 
-        it 'should be at (3,1,E) after two moves' do
+    	    it 'should be at (3,1,E) after two moves' do
 
-            #act
-            @taxi.move_forward
-            @taxi.move_forward
+    	        #act
+    	        taxi.move_forward
+    	        taxi.move_forward
 
-            # assert
-            expect(@taxi.position).to eql([3,1,:E])
-        end
+    	        # assert
+    	        expect(taxi.position).to eql([3,1,:E])
+    	    end
+    	end
     end
 
     describe 'turn right' do
