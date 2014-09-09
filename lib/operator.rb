37eacd653b6241ae5ad827b98d15b0c858f9b6e2
duketon <michael.kendra@iinet.net.au>
2014-09-09 16:20:26 +1000

    Refractor command_handler method to work with the taxi object and commands array.

--- lib/operator.rb
+++ lib/operator.rb
@@ -1,22 +1,34 @@
 class Operator
 
-	def initialize
-		location = parse_location
-		commands = parse_commands
-		
-		@x = location[0]
-		@y = location[1]
-		@facing = location[2]
+	#def initialize
+	#	location = parse_location
+	#	commands = parse_commands
+	#	
+	#	@x = location[0]
+	#	@y = location[1]
+	#	@facing = location[2]
+	#
+	#	@taxi = Taxi.new @x, @y, @facing
+	#	command_handler(@taxi, commands)
+	#end
 
-		@taxi = Taxi.new @x, @y, @facing
-		command_handler!(commands)
-	end
-	
 	def command_handler(taxi,command_string)
 
 		command_string.each_char {|c| excecute_command(taxi, c)}
 		
 		return taxi.stop
+	end
+
+	def parse_location
+		coordinates = ARGV[0].split(',')
+		x = coordinates[0].to_i
+		y = coordinates[1].to_i
+		facing = coordinates[2].to_sym
+		[x, y, facing]
+	end
+
+	def parse_commands
+		commands = ARGV[1].split("")
 	end
 
 	private
@@ -32,19 +44,6 @@ class Operator
 			when 'B'
 				taxi.reverse
 			end
-			i = i + 1 
-		end while i <= command.length
-	end
-
-	def parse_location
-		coordinates = ARGV[0].split(',')
-		x = coordinates[0].to_i
-		y = coordinates[1].to_i
-		facing = coordinates[2].to_sym
-		[x, y, facing]
-	end
-
-	def parse_commands
-		commands = ARGV[1].split("")
+		
 	end
 end
