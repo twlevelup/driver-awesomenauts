2c5ff9a1f49225200932cd888d8a65daabd1b315
Charles <charles.he@triptra.com>
2014-09-20 11:13:19 +1000

    Created a runner file [Charles, Mary, Aaron]

--- lib/console.rb
+++ lib/console.rb
@@ -1,31 +1,38 @@
-require 'operator'
-require 'taxi'
-require 'grid'
+require_relative 'operator'
+require_relative 'taxi'
+require_relative 'grid'
+
 
 class Console
 
-handler = Operator.new
-loop do
-	menu
-	# Get location
 
-	# Create taxi at location
-
-	# Get destination
-
-	# Send to destination
-	handler.progress_all_taxis(taxi)
-
-
-	break if (input == 'Q' || input == 'q')
-
-require_relative 'operator'
+	def initialize(input, output)
+		@input = input
+		@output = output
+	end
 
 def menu
-	puts "Please enter the lacation of taxi eg:1,1,N"
-	taxi_location=gets.chomp
-	handler=Operator.new
+	@output.puts "Please enter the location of taxi eg:1,1,N"
+	taxi_location = @input.gets.chomp
+	handler = Operator.new
 	handler.create_taxi taxi_location
 end
 
+# handler = Operator.new
+# loop do
+# 	menu
+# 	# Get location
+
+# 	# Create taxi at location
+
+# 	# Get destination
+
+# 	# Send to destination
+# 	handler.progress_all_taxis(taxi)
+
+
+# 	break if (input == 'Q' || input == 'q')
+# end
+
 end
+
