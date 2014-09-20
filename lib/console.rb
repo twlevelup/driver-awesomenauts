8fb56c8ac50191bc11b6259e7408af973e7e363c
duketon <michael.kendra@iinet.net.au>
2014-09-20 10:53:15 +1000

    Add skeleton for console class

---
+++ lib/console.rb
@@ -0,0 +1,19 @@
+require 'operator'
+require 'taxi'
+require 'grid'
+
+
+handler = Operator.new
+loop do
+	# Get location
+
+	# Create taxi at location
+
+	# Get destination
+
+	# Send to destination
+	handler.progress_all_taxis(taxi)
+
+
+	break if (input == 'Q' || input == 'q')
+end
